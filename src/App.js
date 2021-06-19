c1003694cb34b228525f80b051552b0a585eaeb6
DoParkEQ <do.park@eqworks.com>
2021-06-19 12:59:24 -0400

    App - update vital checker logic & trim vitalhistory with the rollback number

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import Typography from './components/Typography';
 import Widget from './components/Widget';
+import emergencyAudio from './assets/warning.ogg';
+
 const SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const CHT_UUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 const CONFIG = {
@@ -12,7 +14,6 @@ const CONFIG = {
 const VITAL = 8;
 const GRAPH = 7;
 const TIME_INTERVAL = 1000;
-const STATUS_INTERVAL = 5000;
 const vitalHistory = [];
 const signal = [];
 const rollbackNum = 5;
@@ -20,9 +21,11 @@ const rollbackNum = 5;
 const getTime = (startTime) => {
   const endTime = new Date();
   const elapsedTime = endTime - startTime;
-  const s = Math.round(elapsedTime / 1000) % 60;
-  const m = Math.round(elapsedTime / 60000) % 60;
-  const h = Math.round(elapsedTime / 360000) % 60;
+  const s = parseInt(elapsedTime / 1000) % 60;
+  const m = parseInt(elapsedTime / 60000) % 60;
+  const h = parseInt(elapsedTime / 3600000) % 24;
+  (elapsedTime);
+  (m, h);
   const prefix = '0';
   const time = `${h < 10 ? prefix + h : h}:${m < 10 ? prefix + m : m}:${s < 10 ? prefix + s : s}`;
   return time;
@@ -45,20 +48,24 @@ const widgets = [{
   description: 'Elapsed Time 🕒',
 }];
 
+const audio = new Audio(emergencyAudio);
+const defaultOxyData = { spo2: 0, heartRate: 0, heartGraph: [], elapsedTime: '00:00:00' };
 const App = () => {
-  const [oxyData, setOxyData] = useState({ spo2: 0, heartRate: 0, heartGraph: [], elapsedTime: '00:00:00' });
+  const [isEmergency, setIsEmergency] = useState(false);
+  const [oxyData, setOxyData] = useState(defaultOxyData);
   const [timer, setTimer] = useState(null);
   // const [elapsedTime, setElapsedTime] = useState('00:00:00');
   // const [vitals, setVitals] = useState({spo2: 0, heartRate: 0, graph: []})
   const [isConnected, setIsConnected] = useState(false);
   const [deviceName, setDeviceName] = useState('No devices are connected X');
+  const [openModal, setOpenModal] = useState(false);
 
   const handleNotifications = (e) => {
     const value = e.target.value;
-    const FLAG = 170;
+    const breakPoint = 170;
     for (let i = 0; i < value.byteLength; i++) {
       const count = value.getUint8(i);
-      if (count === FLAG && signal.length > 0) {
+      if (count === breakPoint && signal.length > 0) {
         const identifier = signal[3];
         if (identifier === GRAPH) {
           setOxyData(prev => ({ ...prev, heartGraph: signal.slice(5, 10) }));
@@ -78,6 +85,7 @@ const App = () => {
     setIsConnected(false);
     setDeviceName('No devices are connected');
     setTimer(false);
+    setOxyData(defaultOxyData);
   };
 
   const subscribeBLE = async () => {
@@ -93,7 +101,7 @@ const App = () => {
       const oximetry = await characteristic.startNotifications();
       oximetry.addEventListener('characteristicvaluechanged', handleNotifications);
 
-    } catch(error) {
+    } catch (error) {
       alert(error);
     }
   };
@@ -108,59 +116,70 @@ const App = () => {
           return { ...prev, elapsedTime: elapsedTime };
         });
       }, TIME_INTERVAL);
-
       return () => {
         clearInterval(currentTime);
       };
     }
   }, [isConnected, timer]);
 
-  // const isUserInComa = (count, target) => {
-  //   const { heartRate: heartRate_A, spo2: spo2_A } = vitalHistory[count - 1]
+  const checkVitalAnormalies = () => {
+    const testingRange = [...vitalHistory];
+    const lastVital = testingRange.pop();
+    const test1 = testingRange.every(({ heartRate, spo2 }) =>
+      heartRate === lastVital.heartRate &&
+          spo2 === lastVital.spo2);
 
-  //   for (let i = count - 2; i > target; i--) {
-  //     const { heartRate: heartRate_B, spo2: spo2_B } = vitalHistory[i]
-  //     if (heartRate_A === heartRate_B && spo2_A === spo2_B) {
-  //       return true;
-  //     }
-  //   }
-  //   return false;
-  // }
+    const test2 = testingRange.some(({ heartRate, spo2 }) => heartRate > 140 || heartRate < 40 || spo2 < 80);
+    console.table(test1, test2);
+    return test1 || test2;
+  };
 
   useEffect(() => {
     const len = vitalHistory.length;
     if (len > rollbackNum) {
-      const lastVital = vitalHistory[len - 1];
-      const isUserInComa = vitalHistory.slice(rollbackNum * -1)
-        .every(({ heartRate, spo2 }) =>
-          heartRate === lastVital.heartRate &&
-          spo2 === lastVital.spo2);
+      const isUserInComa = checkVitalAnormalies();
+      setIsEmergency(isUserInComa);
+      vitalHistory.shift();
+      console.table(vitalHistory);
     }
-  }, [vitalHistory.length]);
+  }, [JSON.stringify(vitalHistory)]);
 
-  // useEffect(() => {
-  //   const statusChecker = setInterval(() => {
-  //     vitalHistory.push(oxyData);
-  //     const len = vitalHistory.length
-  //     if (len > 1) {
-  //       const { heartRate: heartRate_A, spo2: spo2_A } = vitalHistory[len - 1]
-  //       const { heartRate: heartRate_B, spo2: spo2_B } = vitalHistory[len - 2]
-  //       console.log(vitalHistory)
-  //       if (heartRate_A === heartRate_B && spo2_A === spo2_B) {
-  //         console.log('warning! the user is in coma.');
-  //       }
-  //     } else {
-  //       clearInterval(statusChecker)
-  //     }
-  //   }, STATUS_INTERVAL)
-  //   return () => {
-  //     clearInterval(statusChecker)
-  //   }
-  // },[oxyData])
 
+
+  useEffect(() => {
+    if (isEmergency) {
+      setOpenModal(true);
+    }
+  },[isEmergency]);
+
+  const onModalClose = () => {
+    setIsEmergency(false);
+    setOpenModal(false);
+  };
+
+  if (openModal) {
+    audio.play();
+  }
 
   return (
-    <div>
+    <>
+      {openModal && <div className='modal__background'>
+        <div className='modal__container'>
+          <div className='modal'>
+            <div>
+              <Typography variant='title'>Emergency</Typography>
+              <Typography variant='title'>응급상황</Typography>
+            </div>
+            <div>
+              <Typography variant='subtitle'>The user's vital sign is stopped. Call 911 if the user is in coma.<br />Do CPR until paramedics arrive.</Typography>
+              <Typography variant='subtitle'>착용자의 바이탈 사인이 멈췄습니다. 의식이 없다면 911에 연락하세요.<br />구급대원이 도착할 때 까지 심폐소생술을 해주세요.</Typography>
+            </div>
+            <div>
+              <button className='btn__primary' onClick={()=>onModalClose()}>착용자는 괜찮습니다. 알람을 끕니다.</button>
+            </div>
+          </div>
+        </div>
+      </div>}
       <div className='status-bar'>
         <div className={isConnected ? 'status-bar__prompt--success' : 'status-bar__prompt--warning'}>
           <Typography variant='body'>{deviceName}</Typography>
@@ -169,9 +188,8 @@ const App = () => {
       </div>
       <div className='widget__container'>
         {widgets.map(({ accessor, unit, description }, idx) => <Widget key={idx} value={oxyData[accessor]} unit={unit} description={description} />)}
-
       </div>
-    </div>
+    </>
   );
 };
 
