8dc0e5f79ea23e8fd5ee12839ed44c96a7f6bb99
DoParkEQ <do.park@eqworks.com>
2021-06-19 18:15:47 -0400

    refactoring WIP: code structure  to follow MVC pattern

---
+++ src/controller/Controller.js
@@ -0,0 +1,175 @@
+import React, { useState, useEffect } from 'react';
+import MainTest from '../view/MainTest'
+
+const TIME_INTERVAL = 1000;
+const signal = [];
+const rollbackNum = 5;
+
+const defaultUserStatus = {
+  isConnected: false,
+  isEmergency: false,
+  deviceName: null,
+  startTime: null,
+}
+
+const defaultOxyData = {
+  spo2: 0,
+  heartRate: 0,
+  heartGraph: [],
+  elapsedTime: '00:00:00'
+}
+
+class vitalHistory {
+  constructor(userID) {
+    this.userID = userID;
+    this.storage = [];
+  }
+
+  add(data) {
+    this.storage.push(data);
+  }
+
+  get() {
+    return this.storage
+  }
+}
+
+const userVital = new vitalHistory('Do Park')
+
+const getTime = (startTime) => {
+  const endTime = new Date();
+  const elapsedTime = endTime - startTime;
+  const s = parseInt(elapsedTime / 1000) % 60;
+  const m = parseInt(elapsedTime / 60000) % 60;
+  const h = parseInt(elapsedTime / 3600000) % 24;
+  (elapsedTime);
+  (m, h);
+  const prefix = '0';
+  const time = `${h < 10 ? prefix + h : h}:${m < 10 ? prefix + m : m}:${s < 10 ? prefix + s : s}`;
+  return time;
+
+};
+
+const checkVitalAnomalies = () => {
+  const HR_MAX = 140;
+  const HR_MIN = 40;
+  const SPO2_MIN = 80;
+  const testingRange = [...userVital];
+  const lastVital = testingRange.pop();
+
+  const test1 = testingRange.every(({ heartRate, spo2 }) =>
+    heartRate === lastVital.heartRate &&
+        spo2 === lastVital.spo2);
+
+  const test2 = testingRange.some(({ heartRate, spo2 }) => HR_MAX > 140 || HR_MIN < 40 || SPO2_MIN < 80);
+
+  return test1 || test2;
+};
+
+const Controller = () => {
+
+  const [userStatus, setUserStatus] = useState(defaultUserStatus)
+  const [oxyData, setOxyData] = useState(defaultOxyData)
+
+  useEffect(() => {
+    if (userStatus.isConnected && userStatus.startTime) {
+      const currentTime = setInterval(() => {
+
+        const elapsedTime = getTime(userStatus.startTime);
+
+        setOxyData(prev => {
+          userVital.add(prev);
+          return { ...prev, elapsedTime: elapsedTime };
+        });
+
+      }, TIME_INTERVAL);
+
+      return () => {
+        clearInterval(currentTime);
+      };
+    }
+
+  }, [userStatus.isConnected, userStatus.startTime]);
+
+
+  useEffect(() => {
+    const len = userVital.length;
+    if (len > rollbackNum) {
+      const isUserInComa = checkVitalAnomalies();
+      setOxyData(prev => ({ ...prev, isEmergency: isUserInComa }));
+      console.table(userVital);
+    }
+  }, [JSON.stringify(userVital)]);
+
+  console.table(userVital.storage)
+  const onConnected = (device) => {
+    setUserStatus(prev =>
+    ({
+      ...prev,
+      isConnected: true,
+      deviceName: device.name,
+      startTime: new Date()
+    }))
+  }
+
+  const onDisconnected = () => {
+    setUserStatus(prev =>
+    ({
+      ...prev,
+      isConnected: false,
+      deviceName: null,
+      startTime: null
+    }))
+  }
+
+  //This belongs to Model
+  const handleNotifications = (e) => {
+    const value = e.target.value;
+    const BREAK_POINT = 170;
+    const GRAPH = 7;
+    const VITAL = 8;
+    for (let i = 0; i < value.byteLength; i++) {
+      const count = value.getUint8(i);
+      if (count === BREAK_POINT && signal.length > 0) {
+        const identifier = signal[3];
+        if (identifier === GRAPH) {
+          setOxyData(prev => ({ ...prev, heartGraph: signal.slice(5, 10) }));
+        }
+        if (identifier === VITAL) {
+          setOxyData(prev => ({ ...prev, spo2: signal[5], heartRate: signal[6] }));
+        }
+        signal.length = 0;
+      }
+      signal.push(count);
+    }
+  };
+
+  const onSubscribe = async () => {
+    const SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+    const CHT_UUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+    const CONFIG = {
+      filters: [{
+        services: [SERVICE_UUID],
+      }],
+    };
+
+    try {
+      const device = await navigator.bluetooth.requestDevice(CONFIG);
+      onConnected(device)
+      device.addEventListener('gattserverdisconnected', onDisconnected);
+      const server = await device.gatt.connect();
+      const service = await server.getPrimaryService(SERVICE_UUID);
+      const characteristic = await service.getCharacteristic(CHT_UUID);
+      const oximetry = await characteristic.startNotifications();
+      oximetry.addEventListener('characteristicvaluechanged', handleNotifications);
+
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+
+  return <MainTest setUserStatus={setUserStatus} onSubscribe={onSubscribe} userStatus={userStatus} oxyData={oxyData}/>;
+};
+
+export default Controller;
---
+++ src/view/MainTest.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import Typography from '../components/Typography';
+import Widget from '../components/Widget';
+import emergencyAudio from '../assets/warning.ogg';
+
+const widgets = [{
+  accessor: 'heartRate',
+  unit: 'bpm',
+  description: 'Heart Rate 💖',
+},
+{
+  accessor: 'spo2',
+  unit: '%',
+  description: 'SPO2 💨',
+},
+{
+  accessor: 'elapsedTime',
+  unit: null,
+  description: 'Elapsed Time 🕒',
+}];
+
+const audio = new Audio(emergencyAudio);
+
+const MainTest = ({onSubscribe, setUserStatus, userStatus, oxyData}) => {
+
+  const { isConnected, isEmergency, deviceName, startTime } = userStatus;
+
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    if (isEmergency) {
+      setOpenModal(true);
+    }
+  },[isEmergency]);
+
+  const onModalClose = () => {
+    setUserStatus(prev => ({...prev, isEmergency: false}))
+    setOpenModal(false);
+  };
+
+  if (openModal) {
+    audio.play();
+  }
+
+  return (
+    <>
+      {openModal && <div className='modal__background'>
+        <div className='modal__container'>
+          <div className='modal'>
+            <div>
+              <Typography variant='title'>Emergency</Typography>
+              <Typography variant='title'>응급상황</Typography>
+            </div>
+            <div>
+              <Typography variant='subtitle'>The user's vital sign is stopped. Call 911 if the user is in coma.<br />Do CPR until paramedics arrive.</Typography>
+              <Typography variant='subtitle'>착용자의 바이탈 사인이 멈췄습니다. 의식이 없다면 911에 연락하세요.<br />구급대원이 도착할 때 까지 심폐소생술을 해주세요.</Typography>
+            </div>
+            <div>
+              <button className='btn__primary' onClick={()=>onModalClose()}>착용자는 괜찮습니다. 알람을 끕니다.</button>
+            </div>
+          </div>
+        </div>
+      </div>}
+      <div className='status-bar'>
+        <div className={isConnected ? 'status-bar__prompt--success' : 'status-bar__prompt--warning'}>
+          <Typography variant='body'>{deviceName || 'Device is not connected'}</Typography>
+        </div>
+        {isConnected ? <p className='status-bar__message body'>Device connected</p>: <button className='btn__connect' onClick={onSubscribe}>Connect</button>}
+      </div>
+      <div className='widget__container'>
+        {widgets.map(({ accessor, unit, description }, idx) => <Widget key={idx} value={oxyData[accessor]} unit={unit} description={description} />)}
+      </div>
+    </>
+  );
+};
+
+
+export default MainTest;
