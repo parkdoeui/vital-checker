6cabda5a5b78ddbb0b06ad806550cb3db646ce97
DoParkEQ <do.park@eqworks.com>
2021-07-05 18:45:35 -0400

    streamline inspector tooltip added; refactored inspection & tooltip internal logic

--- src/components/StreamlineGraph.js
+++ src/components/StreamlineGraph.js
@@ -1,60 +1,78 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as d3 from 'd3';
+import Typography from './Typography';
+import { getAverage } from '../utils';
+import '../styles.css';
+//ix = initial X, iy = initial Y
+const mouseConfig = {
+  position: { ix: 0, iy: 0, x: 0, y: 0, w: 0, h: 0 },
+  isDragged: false,
+};
 
 const StreamlineGraph = ({ data, width, height, config }) => {
 
   const { key, xAxisRange, yAxisRange, style, threshold, screeningMode } = config;
   const { color, strokeWidth } = style;
   const { max, min, unit } = threshold;
-  const [isDragged, setIsDragged] = useState(false);
-  //ix = initial X, iy = initial Y
-  const [mousePos, setMousePos] = useState({ ix: 0, iy: 0, x: 0, y: 0, w: 0, h: 0 });
-  const [inspect, setInspect] = useState(false);
-
+  const [mousePos, setMousePos] = useState(mouseConfig);
+  const [inspector, setInspector] = useState(false);
+  const [tooltip, setTooltip] = useState({ isOpen: false, values: null });
   const svgRef = useRef();
 
   const handleMouseDown = (e) => {
-    setIsDragged(true);
-    setMousePos({ ix: e.offsetX, iy: e.offsetY, x: 0, y: 0, w: 0, h: 0 });
-
+    setMousePos({ isDragged: true, position: { ix: e.offsetX, iy: e.offsetY, x: 0, y: 0, w: 0, h: 0 } });
   };
   const handleMouseUp = (e) => {
-    setIsDragged(false);
     setMousePos(prev => {
-      getInspectedData(prev);
-      return { ix: 0, iy: 0, x: 0, y: 0, w: 0, h: 0 };
+      if (!tooltip.isOpen) {
+        const inspectedData = getInspectedData(prev.position).map(d => d[key]);
+        const tooltipValues = inspectedData.length > 0
+          ? {
+            min: Math.min(...inspectedData) ?? 0,
+            max: Math.max(...inspectedData) ?? 0,
+            avg: getAverage(inspectedData) ?? 0,
+            volume: (inspectedData.length / refinedData.length * 100).toFixed(2) ?? 0,
+          } : null;
+        setTooltip({ values: tooltipValues, position: prev.position, isOpen: true });
+      }
+      return { isDragged: false, position: { ix: 0, iy: 0, x: 0, y: 0, w: 0, h: 0 } };
     });
-
-
   };
+
+  const tooltipOnClose = () => {
+    setTooltip({ values: null, isOpen: false });
+  };
+
   const handleMouseMove = (e) => {
-    if (isDragged) {
+    if (mousePos.isDragged) {
       setMousePos(prev => {
         const currentX = e.offsetX;
         const currentY = e.offsetY;
         const finalMousePos = { x: 0, y: 0, w: 0, h: 0 };
-        if (currentX < prev.ix) {
-          const fx = prev.ix - currentX;
+        if (currentX < prev.position.ix) {
+          const fx = prev.position.ix - currentX;
           finalMousePos.w = fx;
           finalMousePos.x = currentX;
         }
-        if (currentX > prev.ix) {
-          finalMousePos.x = prev.ix;
-          finalMousePos.w = currentX - prev.x;
+        if (currentX > prev.position.ix) {
+          finalMousePos.x = prev.position.ix;
+          finalMousePos.w = currentX - prev.position.x;
         }
-        if (currentY < prev.iy) {
-          const fy = prev.iy - currentY;
+        if (currentY < prev.position.iy) {
+          const fy = prev.position.iy - currentY;
           finalMousePos.y = currentY;
           finalMousePos.h = fy;
         }
-        if (currentY > prev.iy) {
-          finalMousePos.y = prev.iy;
-          finalMousePos.h = currentY - prev.y;
+        if (currentY > prev.position.iy) {
+          finalMousePos.y = prev.position.iy;
+          finalMousePos.h = currentY - prev.position.y;
         }
-        return { ...prev, ...finalMousePos };
+
+        return { ...prev, position: { ...prev.position, ...finalMousePos } };
       });
     }
   };
+
   useEffect(() => {
     if (svgRef.current) {
       svgRef.current.addEventListener('mousedown', handleMouseDown);
@@ -68,7 +86,7 @@ const StreamlineGraph = ({ data, width, height, config }) => {
         svgRef.current.removeEventListener('mousemove', handleMouseMove);
       }
     };
-  }, [svgRef.current, isDragged]);
+  }, [svgRef.current, mousePos.isDragged]);
 
   const refinedData = data.filter(d => d[key] !== 0);
 
@@ -79,10 +97,6 @@ const StreamlineGraph = ({ data, width, height, config }) => {
   const yScale = d3.scaleLinear()
     .domain(yAxisRange)
     .range([height, 0]);
-
-  const invertedYScale = d3.scaleLinear()
-    .domain([0, height])
-    .range([0, height]);
 
   const lineGenerator = d3.line()
     .x((d, i) => xScale(i))
@@ -103,13 +117,24 @@ const StreamlineGraph = ({ data, width, height, config }) => {
     const startY = yScale.invert(pos.y);
     const endX = xScale.invert(pos.x + pos.w);
     const endY = yScale.invert(pos.y + pos.h);
-    console.log(refinedData.filter((d, i) => d[key] < startY && d[key] > endY && i > startX && i < endX));
+    return refinedData.filter((d, i) => d[key] < startY && d[key] > endY && i > startX && i < endX);
+
   };
 
-
   return (
-    <div>
-      <button onClick={() => setInspect(!inspect)}>{inspect ? 'uninspect' : 'inspect'}</button>
+    <div style={{ position: 'relative' }}>
+      {tooltip.isOpen && <div className='streamline__tooltip' style={{ position: 'absolute', left: tooltip.position.x, top: tooltip.position.y }}>
+        {tooltip.values !== null ? <>
+          <Typography variant='body-bold'>Inspected data</Typography>
+          <Typography variant='body'>{`Max: ${tooltip.values.max} ${unit}`}</Typography>
+          <Typography variant='body'>{`Min: ${tooltip.values.min} ${unit}`}</Typography>
+          <Typography variant='body'>{`Average: ${tooltip.values.avg} ${unit}`}</Typography>
+          <Typography variant='body'>{`Volume: ${tooltip.values.volume} %`}</Typography>
+        </>
+          : <Typography>Data not available</Typography>}
+        <button className='btn__tooltip--close' onClick={tooltipOnClose}>Close</button>
+      </div>}
+      <button onClick={() => setInspector(!inspector)}>{inspector ? 'Uninspect' : 'Inspect'}</button>
       <svg ref={svgRef} width={width} height={height}>
         <defs>
           <linearGradient id={`gradient-${key}`} gradientTransform="rotate(90)">
@@ -117,9 +142,9 @@ const StreamlineGraph = ({ data, width, height, config }) => {
             <stop offset='50%' stopColor={`${color}00`} />
           </linearGradient>
         </defs>
-        <rect x={mousePos.x} stroke={color} fill='rgba(0,0,0,0)' strokeDasharray='5 1' y={mousePos.y} width={mousePos.w} height={mousePos.h} />
-        <rect x={mousePos.x} fill={color} opacity='0.1' y={mousePos.y} width={mousePos.w} height={mousePos.h} />
-        {!inspect && thresholdLines.max && <g>
+        <rect x={mousePos.position.x} stroke={color} fill='rgba(0,0,0,0)' strokeDasharray='5 1' y={mousePos.position.y} width={mousePos.position.w} height={mousePos.position.h} />
+        <rect x={mousePos.position.x} fill={color} opacity='0.1' y={mousePos.position.y} width={mousePos.position.w} height={mousePos.position.h} />
+        {!inspector && thresholdLines.max && <g>
           <path id={`max-${key}`} strokeDasharray="10,10" strokeWidth='2' stroke='#ccc' fill='none' strokeWidth='1' d={thresholdLines.max} />
           <text dy='-10' style={{
             fontSize: '12px',
@@ -128,7 +153,7 @@ const StreamlineGraph = ({ data, width, height, config }) => {
             <textPath xlinkHref={`#max-${key}`}>{`maximum ${max}${unit}`}</textPath>
           </text>
         </g>}
-        {!inspect && thresholdLines.min && <g onMouseDown={(e) => screeningMode && onDrag(e, 'min')}>
+        {!inspector && thresholdLines.min && <g>
           <path id={`min-${key}`} strokeDasharray="10,10" strokeWidth='2' stroke='#ccc' fill='none' strokeWidth='1' d={thresholdLines.min} />
           <text dy='20' style={{
             fontSize: '12px',
@@ -139,7 +164,7 @@ const StreamlineGraph = ({ data, width, height, config }) => {
         </g>}
         <path stroke='rbga(255,255,255,0)' fill={`url(#gradient-${key})`} d={`M0,${height} ` + path + ` v${height} L0,${height}`} />
         <path stroke={color} fill='none' strokeWidth={strokeWidth} d={path} />
-        {inspect && refinedData.map((d, i) => <circle cx={xScale(i)} cy={yScale(d[key])} r={strokeWidth} fill='white' />)}
+        {inspector && refinedData.map((d, i) => <circle key={i} cx={xScale(i)} cy={yScale(d[key])} r={strokeWidth} fill='white' />)}
       </svg>
     </div>
   );
