d96a76d6ea6c2976af2e6e1e4140a9baa60f220f
DoParkEQ <do.park@eqworks.com>
2021-07-06 16:01:49 -0400

    setup express and mongoose; created base routing

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { getVitals, createVitals, updateVitals, deleteVitals } from './controllers/userVitalctrl.js'
+
+const app = express();
+const router = express.Router();
+
+router.get('/', getVitals);
+router.post('/post', createVitals);
+router.patch('/patch/:id', updateVitals);
+router.delete('/delete/:id', deleteVitals);
+
+
+app.use('/vitals', router);
+
+const USER_NAME = 'dopark'
+const PASSWORD = 'mGjGs0miZ3xoQboa'
+const CONNECTION_URL = `mongodb+srv://${USER_NAME}:${PASSWORD}@cluster0.wtkgt.mongodb.net/vital-checker?retryWrites=true&w=majority`
+const PORT = 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
